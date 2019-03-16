6159622141cd20461182df14c87a36c0126d39b2
Kev Wilson <kev.wilson84@gmail.com>
2019-03-16 19:54:10 +0000

    Password checker

--- src/PasswordChecker.ts
+++ src/PasswordChecker.ts
@@ -1,11 +1,14 @@
 import { HaveIBeenPwnedPasswordApi } from './lib/HaveIBeenPwnedPasswordApi'
 import * as requestPromise from 'request-promise'
-const sha1 = require('sha1')
+import sha1 = require('sha1')
 
 export namespace PasswordChecker {
   export async function check (password: string, api: HaveIBeenPwnedPasswordApi = new HaveIBeenPwnedPasswordApi(requestPromise)): Promise<number> {
-    const hash = sha1(password)
+    const hash = sha1(password) as string
     const results = await api.fetchResults(hash)
-    return results.length
+
+    return results.reduce((timesSeen, tuple) => {
+      return (tuple[0] === hash) ? tuple[1] : timesSeen
+    }, 0)
   }
 }
--- test/PasswordCheckerSpec.ts
+++ test/PasswordCheckerSpec.ts
@@ -3,18 +3,46 @@ import { PasswordChecker } from '../src/PasswordChecker'
 import { HaveIBeenPwnedPasswordApi } from '../src/lib/HaveIBeenPwnedPasswordApi'
 import * as testdouble from 'testdouble'
 import * as requestPromise from 'request-promise'
+import sha1 = require('sha1')
 
-const genApiResponse = (expectedHash: string, expectedAmount: number) => {
-  const amountOfResuls = Gen.integerBetween(100, 300)()
-  const generatedResults = new Array(amountOfResuls).map(() => {
-
+const genApiResponse = () => {
+  const amountOfResults = Gen.integerBetween(100, 500)()
+  return [...Array(amountOfResults)].map(() => {
+    return [genHash(), Gen.integerBetween(1, 500)()]
   })
 }
 
+const genResponseWith = (expectedHash: string, expectedAmount: number) => {
+  const generatedResults = genApiResponse()
+  const randomIndex = Gen.integerBetween(0, generatedResults.length - 1)()
+  generatedResults[randomIndex] = [expectedHash, expectedAmount]
+  return generatedResults
+}
+
+const genHash = () => {
+  return sha1(Gen.word().concat(Gen.word()))
+}
+
 describe('PasswordChecker', () => {
-  verify.it('should return the correct number of times reported by hibp', Gen.word, async (password) => {
-    const mockApi = testdouble.object(new HaveIBeenPwnedPasswordApi(requestPromise))
-    testdouble.when(mockApi.fetchResults(testdouble.matchers.anything())).thenResolve()
-    await PasswordChecker.check(password, mockApi)
-  })
+  verify.it('should return the correct number of times if reported by hibp',
+    Gen.word, Gen.integerBetween(1, 500), async (password, amount) => {
+      console.log(`Looking for ${amount}`)
+      const hash = sha1(password) as string
+      const mockApi = testdouble.object(new HaveIBeenPwnedPasswordApi(requestPromise))
+      testdouble.when(
+        mockApi.fetchResults(testdouble.matchers.anything())
+      ).thenResolve(genResponseWith(hash, amount))
+
+      return PasswordChecker.check(password, mockApi).should.eventually.eql(amount)
+    })
+
+  verify.it('should return 0 if not reported by hibp',
+    Gen.word, async (password) => {
+      const mockApi = testdouble.object(new HaveIBeenPwnedPasswordApi(requestPromise))
+      testdouble.when(
+        mockApi.fetchResults(testdouble.matchers.anything())
+      ).thenResolve(genApiResponse())
+
+      return PasswordChecker.check(password, mockApi).should.eventually.eql(0)
+    })
 })
